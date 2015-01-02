d61d6f2183f45f0eb845d5a25dd7ec6e146fabf2
Ben Lucato <ben.lucato@enett.com>
2015-01-02 22:14:52 +1100

    Fix syntax error.

--- install.py
+++ install.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
 	target_os = sys.argv[1]
 	home_dir = sys.argv[2]
 
-	os_agnostic_dotfiles = get_dotfiles(os.path.join(__file__), '.')
-	os_specific_dotfiles = get_dotfiles(os.path.join(__file__), target_os)
+	os_agnostic_dotfiles = get_dotfiles(os.path.join(__file__, '.'))
+	os_specific_dotfiles = get_dotfiles(os.path.join(__file__, target_os))
 
 	for dotfile in os_agnostic_dotfiles:
 		shutil.copy(dotfile, home_dir)
