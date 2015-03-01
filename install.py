f978d477d8b0d6dcf7e8ddcfd8233cfdfa1f2a80
Ben Lucato <ben.lucato@enett.com>
2015-03-01 14:47:29 +1100

    Improvements.

--- install.py
+++ install.py
@@ -15,15 +15,11 @@ def get_dotfiles(dirname):
 
 
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		raise RuntimeError('USAGE: python install.py')
-
-	target_os = sys.argv[1]
 	home_dir = os.path.expanduser('~')
 
 	script_location = os.path.dirname(os.path.realpath(__file__))
 	os_agnostic_dotfiles = get_dotfiles(script_location)
-	os_specific_dotfiles = get_dotfiles(os.path.join(script_location, target_os))
+	os_specific_dotfiles = get_dotfiles(os.path.join(script_location, os.name))
 
 	for dotfile in os_specific_dotfiles:
 		shutil.copy(dotfile, home_dir)		
